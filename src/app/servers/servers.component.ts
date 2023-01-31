4a45d7c4c7d579470af7537db53f372b45350dae
Akash Joshi <akashjoshirm.aj@gmail.com>
2023-01-31 13:19:18 +0530

    Learned Inline Template of Component

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  // templateUrl: './servers.component.html',
+  // template : '<app-server></app-server><app-server></app-server>',
+  template : `
+  <app-server></app-server>
+  <p> This is from app-servers inline HTML </p>
+  <button>Click here </button>
+  `,
+
+
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
