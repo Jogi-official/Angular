739fd83857bc3e9f721a58b90ba44dd5ebd12d40
Akash Joshi <akashjoshirm.aj@gmail.com>
2023-01-31 11:18:59 +0530

    Component Using CLI, Nesting of Comp.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ServersComponent,
     ServerComponent
+    
   ],
   imports: [
     BrowserModule
