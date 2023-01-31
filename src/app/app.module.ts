ecbcf0c9b0373e6b54f4f8faf07811efe7c6fb39
Akash Joshi <akashjoshirm.aj@gmail.com>
2023-01-31 14:28:03 +0530

    Learned Selector Manipulation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './warning/warning.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ServersComponent,
-    ServerComponent
+    ServerComponent,
+    WarningComponent
     
   ],
   imports: [
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,17 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  // templateUrl: './servers.component.html',
+  // selector: '[app-servers]',
+  selector : '.app-servers',
+  templateUrl: './servers.component.html',
   // template : '<app-server></app-server><app-server></app-server>',
-  template : `
-  <app-server></app-server>
-  <p> This is from app-servers inline HTML </p>
-  <button>Click here </button>
-  `,
+  // template : `
+  // <app-server></app-server>
+  // <p> This is from app-servers inline HTML </p>
+  // <button>Click here </button>
+  // `,
 
 
-  styleUrls: ['./servers.component.css']
+  styles : [`
+      h1{
+        color : blue;
+      }
+  `]
 })
 export class ServersComponent {
 
