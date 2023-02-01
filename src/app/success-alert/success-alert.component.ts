1ca7aa359be0070408ec3a70302940d4240efb2a
Akash Joshi <akashjoshirm.aj@gmail.com>
2023-02-01 14:29:16 +0530

    Completed Second Assignment

--- src/app/success-alert/success-alert.component.ts
+++ src/app/success-alert/success-alert.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./success-alert.component.css']
 })
 export class SuccessAlertComponent {
-
+      username = "";
+      onClickReset(){
+        this.username = "";
+      }
 }
