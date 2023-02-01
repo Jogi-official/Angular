ebd1ecfdcb4ec54ea443620266518b7c23eb5603
Akash Joshi <akashjoshirm.aj@gmail.com>
2023-02-01 11:20:09 +0530

    Understood String Interpolation- Data Binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,5 +6,10 @@ import { Component } from "@angular/core";
     styleUrls : ['./server.component.css']
 })
 export class ServerComponent{
+    serverId  = 10
+    serverStatus = 'Offline'
 
-}
+    getServerStatus(){
+        return this.serverStatus;
+    }
+}   
