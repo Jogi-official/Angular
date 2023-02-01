774d77e9470526cd61e48f127c6d653f43e4b176
Akash Joshi <akashjoshirm.aj@gmail.com>
2023-02-01 10:22:47 +0530

    Deleted Clutter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { WarningComponent } from './warning/warning.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
@@ -14,7 +13,6 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component';
     AppComponent,
     ServersComponent,
     ServerComponent,
-    WarningComponent,
     WarningAlertComponent,
     SuccessAlertComponent
     
