e9ee877b2e6c423292accbdb483c5828c0565deb
Akash Joshi <akashjoshirm.aj@gmail.com>
2023-02-02 13:46:18 +0530

    Started Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,28 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
-
-
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 @NgModule({
   declarations: [
     AppComponent,
     ServersComponent,
     ServerComponent,
     WarningAlertComponent,
-    SuccessAlertComponent
-    
+    SuccessAlertComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    ShoppingEditComponent,
+    ShoppingListComponent
+
   ],
   imports: [
     BrowserModule,
