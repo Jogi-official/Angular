7c802d6c0ed7163e551e5c0aad67e237a4857f52
Akash Joshi <akashjoshirm.aj@gmail.com>
2023-02-02 12:01:36 +0530

    Refactored Code

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -10,6 +10,7 @@ export class WarningAlertComponent {
   log = [];
   onToggleDisplay(){
     this.showSecret = !this.showSecret;
-    this.log.push(this.log.length+1);
+    // this.log.push(this.log.length+1);
+    this.log.push(new Date())
   }
 }
