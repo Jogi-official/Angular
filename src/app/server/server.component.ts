aa6c45ad74282683ff53b3ae6a41c04984bc3798
Akash Joshi <akashjoshirm.aj@gmail.com>
2023-02-02 11:13:39 +0530

    Understood ngStyle

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,8 +8,14 @@ import { Component } from "@angular/core";
 export class ServerComponent{
     serverId  = 10
     serverStatus = 'Offline'
-
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'Online' : 'Offline';
+    }
     getServerStatus(){
         return this.serverStatus;
     }
-}   
+
+    getColor(){
+      return this.serverStatus === 'Online' ? 'green' : 'red';
+    }
+}
