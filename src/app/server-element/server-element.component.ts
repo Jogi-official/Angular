73085264ee6ac6c64560351feea27faad6b82d9f
Akash Joshi <akashjoshirm.aj@gmail.com>
2023-02-06 11:02:54 +0530

    Understood binding to custom properties

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
-import { OnInit , Input} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent {
-  @Input() element : {type : string; name : string , content : string};
+export class ServerElementComponent implements OnInit {
+  @Input() element : {type : string, name : string , content: string};
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
 }
