d986d376fd8901b25f01dc79562e6428a3eaae3b
Akash Joshi <akashjoshirm.aj@gmail.com>
2023-02-07 14:57:38 +0530

    Used Event Binding in Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,19 +25,27 @@ export class AppComponent {
 //        });
 //      }
 
-oddNumbers : number [] = [];
-evenNumbers : number [] = [];
+// oddNumbers : number [] = [];
+// evenNumbers : number [] = [];
 
 
-onIntervalFired(firedNumber : number) {
-    if(firedNumber%2 === 0 ){
-      this.evenNumbers.push(firedNumber);
-    }
-    else{
-      this.oddNumbers.push(firedNumber);
-    }
+// onIntervalFired(firedNumber : number) {
+//     if(firedNumber%2 === 0 ){
+//       this.evenNumbers.push(firedNumber);
+//     }
+//     else{
+//       this.oddNumbers.push(firedNumber);
+//     }
 
 
+// }
+
+
+
+//MAIN CODE
+loadedFeature = 'recipe';
+onNavigate(feature) {
+  this.loadedFeature = feature;
 }
 
 
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes : Recipe[] = [
     new Recipe('Chicken', 'This is Chicken Biryani' , 'https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80'),
     new Recipe('XYUX', 'Chicken dadsad' , 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVPTVxKFwqL3_wgTS__Mw49Zkfuet0eGaepw&usqp=CAU'),
@@ -15,6 +16,9 @@ export class RecipeListComponent {
   ];
   constructor(){
 
+  }
+  onRecipeSelected(recipe : Recipe){
+      this.recipeWasSelected.emit(recipe);
   }
 
 }
