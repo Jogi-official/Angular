99344bf25f4a5fbc41a8ba8cee397ff997e43905
Akash Joshi <akashjoshirm.aj@gmail.com>
2023-02-17 00:45:33 +0530

    Added routing with indivual server

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { ServersService } from '../server.service';
+import { ServersService  } from '../server.service';
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
@@ -10,16 +12,24 @@ export class EditServerComponent {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  query_params = {};
+  fragments = {};
+
+  constructor(private serversService: ServersService ,
+    private route : ActivatedRoute ) { }
 
   ngOnInit() {
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
+    this.route.queryParams.subscribe()
+    this.route.fragment.subscribe()
+
   }
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
   }
+
 
 }
--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit} from '@angular/core';
+import { ActivatedRoute , Params } from '@angular/router';
 
 import { ServersService } from '../server.service';
 
@@ -10,9 +11,16 @@ import { ServersService } from '../server.service';
 export class ServerComponent {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService ,
+    private route : ActivatedRoute) { }
 
   ngOnInit() {
+    const id = +this.route.snapshot.params['id'];
     this.server = this.serversService.getServer(1);
+    this.route.params.subscribe(
+      (params : Params)=>{
+        this.server = this.serversService.getServer(+params['id']);
+      }
+    );
   }
 }
