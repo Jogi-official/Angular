7f5ccbff0a2b7575123894f83de68049acf2eff9
Akash Joshi <akashjoshirm.aj@gmail.com>
2023-02-16 17:49:49 +0530

    Fetching Route Parameters Reactively using Obserable

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,12 +1,12 @@
 import { Component , OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute , Params} from '@angular/router';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
   user: {id: number, name: string};
 
   constructor(private route : ActivatedRoute ) { }
@@ -16,5 +16,14 @@ export class UserComponent {
       id : this.route.snapshot.params['id'],
       name : this.route.snapshot.params['name']
     };
+
+    this.route.params.subscribe(
+      (params : Params) =>{
+        this.user.id = params['id'];
+        this.user.name = params['name'];
+      }
+    )
   }
+
+
 }
