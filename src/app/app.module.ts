10301696ef765b4bf87babe04d5e6e75736ce78a
Akash Joshi <akashjoshirm.aj@gmail.com>
2023-02-20 17:32:44 +0530

    Added Editing Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import {DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 // import { HomeComponent } from './home/home.component';
 // import { ServersComponent } from './servers/servers.component';
 // import { UsersComponent } from './users/users.component';
@@ -80,6 +81,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
     ShoppingListComponentt,
     DropdownDirective,
     RecipeStartComponent,
+    RecipeEditComponent,
   ],
   imports: [
     BrowserModule,
